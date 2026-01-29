49aef22ed0019d2133844ca0639858e9b4c59a90
MD Software Solutions <sohamdesai6508@gmail.com>
2026-01-29 16:16:21 -0500

    Initial commit with JavaFX project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module org.mp.frontend26 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.mp.frontend26 to javafx.fxml;
+    exports org.mp.frontend26;
+}
